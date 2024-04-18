3755690ec51ac3ea2aaee0d6a967620626f901ac
evolvedshaman <Macrenzae21@gmail.com>
2024-04-18 12:43:07 +0800

    added files

--- public/delete.php
+++ public/delete.php
@@ -14,10 +14,11 @@ if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
         // Set parameters
         $param_id = trim($_POST["product_id"]);
         
-        // Attempt to execute the prepared statement
+        // Attempt to execute the prepared statement  
+        
         if($stmt->execute()){
             // Records deleted successfully. Redirect to landing page
-            header("location: ../index.php");
+            header("location: ../index.php");  
             exit();
         } else{
             echo "Oops! Something went wrong. Please try again later.";
