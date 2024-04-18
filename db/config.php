35aec0821dc7a92bf6952ff47fcfb9fdbb151892
evolvedshaman <Macrenzae21@gmail.com>
2024-04-18 12:27:12 +0800

    added php page

--- db/config.php
+++ db/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'products');
+define('DB_NAME', 'new_bd');
  
 /* Attempt to connect to MySQL database */
 try{
--- db/new_bd.sql
+++ db/new_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2024 at 05:53 AM
+-- Generation Time: Apr 18, 2024 at 06:17 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `products`
+-- Database: `new_bd`
 --
 
 -- --------------------------------------------------------
@@ -29,33 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `products` (
   `product_id` int(11) NOT NULL,
-  `product_thumbnail_link` varchar(255) DEFAULT NULL,
-  `product_name` varchar(255) NOT NULL,
-  `product_description` varchar(100) NOT NULL,
-  `product_retail_price` varchar(255) NOT NULL,
-  `product_date_added` date NOT NULL,
-  `product_updated_date` date DEFAULT NULL
+  `product_name` varchar(100) NOT NULL,
+  `product_details` varchar(255) NOT NULL,
+  `product_retail_price` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
