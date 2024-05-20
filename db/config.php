9b92e01d1de07613eb7e06ac02736bb1829ec47e
evolvedshaman <macrenzae21@gmail.com>
2024-05-20 18:13:44 +0800

    added initial files

--- db/config.php
+++ db/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD', '');
-define('DB_NAME', '');
+define('DB_NAME', 'u593341949_db_cardines');
 
  
 /* Attempt to connect to MySQL database */
