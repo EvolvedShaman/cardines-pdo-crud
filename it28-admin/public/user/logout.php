79615d7a137a5a44472e2405ac01315af8cc62e2
EvolvedShaman <147384406+EvolvedShaman@users.noreply.github.com>
2024-06-03 20:17:09 +0800

    Update logout.php

--- it28-admin/public/user/logout.php
+++ it28-admin/public/user/logout.php
@@ -9,6 +9,6 @@ $_SESSION = array();
 session_destroy();
  
 // Redirect to login page
-header("Location: /baslao-pdo-crud-2/it28-admin/index.php");
+header("Location: /cardines-pdo-crud/it28-admin/index.php");
 exit;
 ?>
